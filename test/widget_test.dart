98bf15a64b10dac3d3b253a2d19b9f5c62da3b29
David RG <davidromerog@gmail.com>
2023-09-05 12:56:39 +0800

    testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_fist_application/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Like increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('No favorites yet.'), findsOneWidget);
+    expect(find.text('You have 1 favourites:'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'Favourites' icon and trigger a frame.
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Like'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('No favorites yet.'), findsNothing);
+    expect(find.text('You have 1 favourites:'), findsOneWidget);
   });
 }
